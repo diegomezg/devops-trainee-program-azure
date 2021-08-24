8d47a2cf410b08e1674d841402aa22741ed8100d
alanmas-cr <alan.acosta@cockroachlabs.com>
2021-08-24 12:41:48 -0500

    restructure: removing courses and adding new courses
    Based on business requirement, for this new Gra Program version we do need to focus in only one cloud (Azure) and power shell.
    
    Main changes:
    - Removing:
    	- AWS Essentials.
    	- Linux Bash Script
    	- Configuration Management
    	- Build tools (maven-gradle)
    	- Jenkins & Pipelines Construction with Code
    - Adding:
    	- Microsoft PowerShell scripting
    	- Microsoft Azure Essentials
    	- Microsoft Azure Architecture
    	- Microsoft Azure DevOps Pipelines
    	- Configuration management with Ansible

--- 04 - Unix shell scripting/live-exercises/loops.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Lists all shell scripts files type"
-# for f in `ls *.sh`; do
-for f in $(ls *.sh)
-do
-    # echo "File $f";
-    file $f
-done
--- 04 - Unix shell scripting/live-exercises/my_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# hola
-echo "escribe una palabra"
-read palabra
-echo "Tu palabra fue $palabra"
